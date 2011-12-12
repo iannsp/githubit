880558f993904a2e8e444cd0b0808c966ea309d4
Your Name <iannsp@gmail.com>
2011-12-12 19:30:18 +0000

    declare ghin

--- ghtype.h
+++ ghtype.h
@@ -1,6 +1,7 @@
 // the ghtype file have the input and output structures used by githubit to transport data.
 
 #define GHURL "https://api.github.com/"
-struct ghin{
-//    area char[20];
-};
+typedef struct{
+    char* area ;
+    char* command[];
+} ghin;
