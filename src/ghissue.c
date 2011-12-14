4aed4364bfc5c33afc643da72bfdcb18ba7dc3b3
Ivo Nascimento <iannsp@gmail.com>
2011-12-14 10:18:04 -0200

    the issue functions

---
+++ src/ghissue.c
@@ -0,0 +1,19 @@
+/** the functions to handler with issue**/
+#include <stdio.h>
+#include "ghtype.h"
+int    ghIssue(ghin param)
+{
+    printf("%s", param.command);
+}
+ghout   list(ghin param)
+{
+    
+}
+ghout   add(ghin param)
+{
+    
+}
+ghout   edit(ghin param)
+{
+    
+}
