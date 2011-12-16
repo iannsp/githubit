18cd5556a9a451c277bf8e31176b0a6767681156
Ivo Nascimento <iannsp@gmail.com>
2011-12-16 03:29:46 -0200

    add data type to curl and param to ghout

--- src/ghtype.h
+++ src/ghtype.h
@@ -1,21 +1,31 @@
 // the ghtype file have the input and output structures used by githubit to transport data.
 
-
 #define GHURL "https://api.github.com/"
-typedef struct{
+
+typedef struct {
+ char *contents;
+ int size;
+} data;
+
+typedef struct ghParam ghParam;
+
+struct ghParam{
     char* name;
     char* value;
-    struct ghParam* next;
-    } ghParam;
+    ghParam* next;
+    };
 typedef struct {
-    char* area ;
-    char* command;
-    ghParam* param  ;
+    char *area ;
+    char *command;
+    ghParam *param  ;
+    char *url;
     } ghin;
 
 typedef struct{
-    
+    char *content;
+    char *url;
     } ghout;
+    
 typedef enum{
     head,
     get,
