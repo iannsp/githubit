33ced6df36195cab02e495337aa172e326fa8c82
Ivo Nascimento <iannsp@gmail.com>
2011-12-16 03:29:03 -0200

    add prototype to getParamValue function

--- src/ghstuff.h
+++ src/ghstuff.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 void hellogh();
 void helphg();
+char* getParamValue(ghParam *param, char *name);
 ghParam* ghcreateParam(int argc, char* argv[]);
 ghParam* create_ghParam(char* name, char* value);
