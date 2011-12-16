1ea3377c8af996b704bbc0e7ad317be69f61fc4b
Ivo Nascimento <iannsp@gmail.com>
2011-12-16 03:27:59 -0200

    add prefix to functions

--- src/ghissue.h
+++ src/ghissue.h
@@ -1,9 +1,5 @@
-/** declare the functions to issue **/
-// usar sprintf para fazer os comandos e recebendo um numero fixo de parametros.
-/*
-support to list all issues, issues from a repo, e just one issue
-*/
-//int    ghIssue(ghin param);
-ghout   list(ghin param);
-ghout   add(ghin param);
-ghout   edit(ghin param);
+ghout   ghissue_list(ghin param);
+ghout   ghissue_mylist(ghin param);
+ghout   ghissue_add(ghin param);
+ghout   ghissue_edit(ghin param);
+void    ghissue_help();
