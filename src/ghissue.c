7d05037cb488492ca951166a34589b2913b5879c
Ivo Nascimento <iannsp@gmail.com>
2011-12-16 19:37:20 -0200

    add message format

--- src/ghissue.c
+++ src/ghissue.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <json/json.h>
 #include <string.h>
 #include "ghtype.h"
 #include "ghcurl.h"
 #include "ghstuff.h"
 #include "ghissue.h"
-
+#include <jansson.h>
+#include "ghjson.h"
+ 
 int    ghIssue(ghin param)
 {
     if ( strcmp(param.command,"add")==0) {
@@ -32,6 +33,12 @@ int    ghIssue(ghin param)
 ghout   ghissue_list(ghin param)
 {
     ghout out;
+    size_t i;
+    const char *title, *body, *state;
+    int id;
+    json_t *json;
+    json_t  *item;
+
     char *user = getParamValue(param.param, "user");
     char *repo = getParamValue(param.param, "repo");
 
@@ -40,10 +47,24 @@ ghout   ghissue_list(ghin param)
         ghissue_help();
         exit(EXIT_FAILURE);
     }
-    param.url = malloc(sizeof(GHURL)+sizeof("repos/")+sizeof(user)+sizeof(repo)+3);
-    sprintf(param.url, "%srepos/%s/%s/issues", GHURL,user,repo );
+    param.url = malloc(sizeof(GHURL)+sizeof("repos/?sort=created&direction=asc")+sizeof(user)+sizeof(repo)+3);
+    sprintf(param.url, "%srepos/%s/%s/issues?sort=created&direction=asc", GHURL,user,repo );
     out = ghExecute(get, param);
-    printf("%s\n", out.content);
+    ghstringparse(&out);
+    for(i = 0; i < json_array_size(out.json); i++)
+    {
+        item = json_array_get(out.json, i);
+        if(!json_is_object(item))
+        {
+            printf("Problemas com os dados.\n");
+            exit(EXIT_FAILURE);
+        }
+        title       = json_string_value(getValueOf(item, "title"));
+        body        = json_string_value(getValueOf(item, "body"));
+        id          = json_integer_value(getValueOf(item, "number"));
+        state       = json_string_value(getValueOf(item, "state"));
+        printf("(%-4s)%4d- %s \n", state,id, title); 
+    }
     return out;
 }
 ghout   ghissue_mylist(ghin param)
