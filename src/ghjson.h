9e90ee19cf96b24d0b5b549d54afc1da63bb0d18
Ivo Nascimento <iannsp@gmail.com>
2011-12-16 19:38:37 -0200

    prototype getJsonItensbyName and getValueOf

--- src/ghjson.h
+++ src/ghjson.h
@@ -1,3 +1,4 @@
 
 void ghstringparse(ghout *ghout);
-json_t* getJsonItens(ghout *out, char *nodename);
+json_t* getJsonItensbyName(ghout *out, char *nodename);
+json_t* getValueOf(json_t *json, char* name);
