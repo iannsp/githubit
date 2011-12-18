e76e8159c9707d6dd226114e73b8ba4c0a5dd9e2
Ivo Nascimento <iannsp@gmail.com>
2011-12-18 19:36:55 -0200

    add ghissue_show prototype

--- src/ghissue.h
+++ src/ghissue.h
@@ -2,4 +2,5 @@ ghout   ghissue_list(ghin param);
 ghout   ghissue_mylist(ghin param);
 ghout   ghissue_add(ghin param);
 ghout   ghissue_edit(ghin param);
+ghout   ghissue_show(ghin param);
 void    ghissue_help();
